60bd0f76bdfe50ad8120ad5adac3b841b567f591
alokyadav686 <alokkumaryadav2799@gmail.com>
2024-11-04 23:42:44 +0530

    profile section added

--- lib/mainscreen/profilescreen.dart
+++ lib/mainscreen/profilescreen.dart
@@ -32,178 +32,211 @@ class _ProfilescreenState extends State<Profilescreen> {
             end: Alignment.bottomRight,
           ),
         ),
-        child: Center(
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        
+            SizedBox(height: 30,),
+        
+            Text("Profile",style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
+        
 
-              SizedBox(height: 30,),
+            SizedBox(height: 50,),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+              // color: Colors.amber,
+                borderRadius: BorderRadius.circular(60),
+                image: DecorationImage(image: AssetImage("assets/images/profile.png"),
+                fit: BoxFit.cover),
 
-              Text("Profile",style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white)),
-
-
-              SizedBox(height: 30,),
-
-                    Expanded(
-                child: Container(
-                  height: 800,
-                  width: double.infinity,
-                  // color: Colors.amber,
-                  child: Center(
-                    child: Column(
-                      children: [
-                        
-                        StreamBuilder<QuerySnapshot>(
-                          stream: firestore,
-                          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                            
-                            if (snapshot.hasError) {
-                              return Text("Some Error");
-                            }
-                            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                              return Text("No Data Available");
-                            }
-                
-                            var userDoc = snapshot.data!.docs[0];
-
-                      return Column(
-                        children: [
-                          ProfileSection(title: "Name : ", value: userDoc['name']),
-                          ProfileSection(title: "Student ID : ", value: userDoc['student id']),
-                          ProfileSection(title: "Roll Number : ", value: userDoc['roll number']),
-                          ProfileSection(title: "Branch : ", value: userDoc['branch']),
-                          ProfileSection(title: "Section : ", value: userDoc['section']),
-                          ProfileSection(title: "Skills : ", value: userDoc['skills']),
-                          ProfileSection(title: "Gender : ", value: userDoc['gender']),
-                        ],
-                      );
-
-                          },
-                        ),
-                
-                        
-                      ],
-                    ),
-                  ),
-                ),
               ),
-
-              Spacer(),
-              Column(
-                children: [
-
-
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => Editprofilescreen()));
-                          },
-                          child: Container(
-                            height: 35,
-                            width: 100,
-                            decoration: BoxDecoration(
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.circular(30)
-                            ),
-                            
-                            child: Center(child: Text("Edit Profile", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Colors.blueAccent),)),
-                          ),
+              
+            ),
+            SizedBox(height: 10,),
+            Container(
+              height: 20,
+              width: 80,
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white60,
+              ),
+              child: Center(child: Text("Edit Profile")),
+            ),
+            
+        
+            SizedBox(height: 25,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Container(
+                      // height: 800,
+                      width: double.infinity,
+                      // color: Colors.blueAccent,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Colors.deepPurpleAccent,Colors.blueAccent
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight
                         ),
-                        SizedBox(height: 20,),
-                        InkWell(
-                          onTap: () {
-                            auth.signOut().then((value){
-
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => Loginscreen()));
-                            }).onError((error, stackTrace) {
-                              utils().toastMessage(error.toString());
-                            });
-                          },
-                          child: Container(
-                            height: 35,
-                            width: 100,
-                            decoration: BoxDecoration(
-                            color: Colors.white70,
-                            borderRadius: BorderRadius.circular(30)
-                            ),
-                            
-                            child: Center(child: Text("Sign Out", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Colors.blueAccent),)),
-                          ),
-                        ),
-                        SizedBox(height: 20,),
-                Container(
-                  height: 60,
-                  width: double.infinity,
-                  color: Colors.white12,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Column(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: InkWell(
-                              onTap: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => Firstscreen()));
-                              },
-                              child: Container(
-                                width: 60,
-                                height: 52,
-                                child: Column(
-                                  children: [
-                                    Icon(Icons.home_outlined,color: Colors.white,),
-                                    Text("Home", style: TextStyle(color: Colors.white),),
-                                  ],
-                                ),
-                              ),
-                            ),
+                          
+                          StreamBuilder<QuerySnapshot>(
+                            stream: firestore,
+                            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                              
+                              if (snapshot.hasError) {
+                                return Text("Some Error");
+                              }
+                              if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                                return Text("No Data Available");
+                              }
+                                      
+                              var userDoc = snapshot.data!.docs[0];
+                      
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Column(
+                            children: [
+                              ProfileSection(title: "Name : ", value: userDoc['name'], icon: Icons.person,),
+                              ProfileSection(title: "Student ID : ", value: userDoc['student id'], icon: Icons.school,),
+                              ProfileSection(title: "Roll Number : ", value: userDoc['roll number'],icon: Icons.card_membership),
+                              ProfileSection(title: "Branch : ", value: userDoc['branch'],icon: Icons.book),
+                              ProfileSection(title: "Section : ", value: userDoc['section'],icon: Icons.diversity_1),
+                              ProfileSection(title: "Skills : ", value: userDoc['skills'],icon: Icons.star),
+                              ProfileSection(title: "Gender : ", value: userDoc['gender'],icon: Icons.transgender),
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: InkWell(
-                              onTap: () {
-                                Navigator.push(context, MaterialPageRoute(builder: (context) => Aboutscreen()));
-                              },
-                              child: Container(
-                                width: 60,
-                                height: 52,
-                                child: Column(
-                                  children: [
-                                    Icon(Icons.info_outline_rounded,color: Colors.white,),
-                                    Text("About", style: TextStyle(color: Colors.white),),
-                                  ],
-                                ),
-                              ),
-                            ),
+                        );
+                      
+                            },
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: InkWell(
-                              onTap: () {
-                
-                                // Navigator.push(context, MaterialPageRoute(builder: (context) => Profilescreen()));
-                              },
-                              child: Container(
-                                width: 60,
-                                height: 52,
-                                child: Column(
-                                  children: [
-                                    Icon(Icons.account_circle_outlined,color: Colors.white,),
-                                    Text("Profile",style: TextStyle(color: Colors.white),),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
+                                      
+                          
                         ],
                       ),
-                    ],
+                    ),
                   ),
+        
+            Spacer(),
+            Column(
+              children: [
+        
+        
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Editprofilescreen()));
+                        },
+                        child: Container(
+                          height: 35,
+                          width: 100,
+                          decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadius.circular(30)
+                          ),
+                          
+                          child: Center(child: Text("Edit Profile", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Colors.blueAccent),)),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      InkWell(
+                        onTap: () {
+                          auth.signOut().then((value){
+        
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => Loginscreen()));
+                          }).onError((error, stackTrace) {
+                            utils().toastMessage(error.toString());
+                          });
+                        },
+                        child: Container(
+                          height: 35,
+                          width: 100,
+                          decoration: BoxDecoration(
+                          color: Colors.white70,
+                          borderRadius: BorderRadius.circular(30)
+                          ),
+                          
+                          child: Center(child: Text("Sign Out", style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: Colors.blueAccent),)),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+              Container(
+                height: 60,
+                width: double.infinity,
+                color: Colors.white12,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => Firstscreen()));
+                            },
+                            child: Container(
+                              width: 60,
+                              height: 52,
+                              child: Column(
+                                children: [
+                                  Icon(Icons.home_outlined,color: Colors.white,),
+                                  Text("Home", style: TextStyle(color: Colors.white),),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => Aboutscreen()));
+                            },
+                            child: Container(
+                              width: 60,
+                              height: 52,
+                              child: Column(
+                                children: [
+                                  Icon(Icons.info_outline_rounded,color: Colors.white,),
+                                  Text("About", style: TextStyle(color: Colors.white),),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: InkWell(
+                            onTap: () {
+              
+                              // Navigator.push(context, MaterialPageRoute(builder: (context) => Profilescreen()));
+                            },
+                            child: Container(
+                              width: 60,
+                              height: 52,
+                              child: Column(
+                                children: [
+                                  Icon(Icons.account_circle_outlined,color: Colors.white,),
+                                  Text("Profile",style: TextStyle(color: Colors.white),),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                ],
               ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -215,8 +248,9 @@ class _ProfilescreenState extends State<Profilescreen> {
 class ProfileSection extends StatefulWidget {
   final String title;
   final String value;
+  final IconData icon;
 
-  const ProfileSection({super.key,required this.title,required this.value});
+  const ProfileSection({super.key,required this.title,required this.value, required this.icon});
 
   @override
   State<ProfileSection> createState() => _ProfileSectionState();
@@ -231,13 +265,15 @@ class _ProfileSectionState extends State<ProfileSection> {
       child: Row(
 
         // crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
+          Icon(widget.icon, color: Colors.white,),
+          SizedBox(width: 20,),
           Text(
             widget.title,
             style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 10,),
+          SizedBox(height: 10,width: 10,),
           Text(
             widget.value,
             style: TextStyle(color: Colors.white, fontSize: 18),
